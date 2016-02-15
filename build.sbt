a08054957da98288f94d91ee460e071721828c3f
ultimate1352 <ultimate1352@gmail.com>
2016-02-15 20:48:07 +0900

    update(): add scafold for cluster

--- build.sbt
+++ build.sbt
@@ -4,4 +4,11 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.1"
+libraryDependencies ++= {
+  Seq("com.typesafe.akka" %% "akka-actor" % "2.4.1",
+    "com.typesafe.akka" %% "akka-remote" % "2.4.1",
+    "com.typesafe.akka" %% "akka-cluster" % "2.4.1",
+    "com.typesafe.akka" %% "akka-cluster-metrics" % "2.4.1",
+    "com.typesafe.akka" %% "akka-cluster-tools" % "2.4.1"
+  )
+}
--- src/main/scala/durian/DurianApplication.scala
+++ src/main/scala/durian/DurianApplication.scala
@@ -1,10 +1,12 @@
 import akka.actor.{Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
-import durian.actors.server.ServerActor
+import durian.actors.server.{SimpleClusterListener, ServerActor}
 
 object DurianApplication extends App {
 //  val config = ConfigFactory.load()
 //  val system = ActorSystem("durian", config.getConfig(""))
   val system = ActorSystem("durian")
   val serverActor = system.actorOf(Props[ServerActor], "serverActor")
+
+  val simpleListener = system.actorOf(Props[SimpleClusterListener], "listener")
 }
